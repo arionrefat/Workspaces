c008c280a3dd8c827eb6ef5c1f04b3c4221ed688
arionrefat <arionrefat@gmail.com>
2020-11-14 21:05:08 +0600

    Updated some configs

---
+++ Javascript/index.js
@@ -0,0 +1,51 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log("Refat");
+
+
+//variable name 
+let firstName = 'Jonas';
+console.log(firstName);
+let las$Name = 'Jonas';
+console.log(las$Name);
+
+let java = true;
+console.log(java);
+
+//console.log(typeof true);
+console.log(typeof java);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+
+java = 'yes';
+console.log(typeof java);
+
+let year;
+console.log(typeof year);
+
+year= 1991;
+console.log(typeof year);
+console.log(typeof null)
+*/
+
+let age = 30;
+age = 31;
+
+console.log(age);
+
+const birthYear = 2020 - 2000;
+const agesicker= 2077 - 2000;
+console.log(birthYear,agesicker);
+
+var job = 'sucking'
+job = 'teacher'
+
+const nameFirst = 'Refatul';
+const nameLast = 'Islam';
+
+console.log(nameFirst + ' ' + nameLast);
+console.log(agesicker > birthYear);
+
+
