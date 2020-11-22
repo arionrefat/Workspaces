5ad4f31c492e3e9b50aa155f6ddb0931c2f39ce0
arionrefat <arionrefat@gmail.com>
2020-11-22 10:12:56 +0600

    added codes

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,51 +1,10 @@
-/*
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
-console.log("Refat");
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthyear = 2000;
+const year = 2020;
 
+const jonas = "I'm " + firstName + ', a ' + (year - birthyear) + ' year old ' + job + '!';
+console.log(jonas);
 
-//variable name 
-let firstName = 'Jonas';
-console.log(firstName);
-let las$Name = 'Jonas';
-console.log(las$Name);
-
-let java = true;
-console.log(java);
-
-//console.log(typeof true);
-console.log(typeof java);
-console.log(typeof 23);
-console.log(typeof 'jonas');
-
-
-java = 'yes';
-console.log(typeof java);
-
-let year;
-console.log(typeof year);
-
-year= 1991;
-console.log(typeof year);
-console.log(typeof null)
-*/
-
-let age = 30;
-age = 31;
-
-console.log(age);
-
-const birthYear = 2020 - 2000;
-const agesicker= 2077 - 2000;
-console.log(birthYear,agesicker);
-
-var job = 'sucking'
-job = 'teacher'
-
-const nameFirst = 'Refatul';
-const nameLast = 'Islam';
-
-console.log(nameFirst + ' ' + nameLast);
-console.log(agesicker > birthYear);
-
-
+const jonasNew = `I'm ${firstName}, a (${year} - ${birthyear}) ${job}`
+console.log(jonasNew);
