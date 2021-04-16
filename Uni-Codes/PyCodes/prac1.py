b4aec2c27854dc4014589c1e00af562302c9da50
arionrefat <arionrefat@gmail.com>
2021-04-16 23:21:01 +0600

    Updated codes

--- Uni-Codes/PyCodes/prac1.py
+++ Uni-Codes/PyCodes/prac1.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+def bin_to_dec(string,i,num):
+
+  if len(string) == i :
+
+    return num
+
+  else:
+
+    if string[i] == '0':
+
+      num=2*num
+
+    else:
+
+      num=2*num+1
+
+  return bin_to_dec(string,i+1,num)
+
