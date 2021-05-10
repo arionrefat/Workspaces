f0816254cae1042534dd26be31043941efdd2f47
arionrefat <arionrefat@gmail.com>
2021-05-10 23:55:07 +0600

    Updated Codes

--- Uni-Codes/Java/Main.java
+++ Uni-Codes/Java/Main.java
@@ -1,3 +1,23 @@
+class Tester {
+    public static void main(String args[]) {
+        int[] array = { 4, -2, 3, -4, 7, 4 };
+        // int[] array = { 2, 3, 4, 4, 9, 5 };
+
+        // KeyIndex keyIndex = new KeyIndex(array);
+        // int[] answer = keyIndex.sort();
+
+        // for (int i : answer) {
+        // System.out.println(i);
+        // }
+        // System.out.println(keyIndex.search(1));
+
+        String[] word = { "12G0", "AB1J0", "ST1E89B8A32", "11060", "ABBY1", "KLKA090", "GHUA991", "GHAA011",
+                "HJLA991" };
+        Hashing hash = new Hashing(word);
+        hash.print();
+    }
+}
+
 class KeyIndex {
     int[] k;
     int min = 0;
@@ -70,12 +90,23 @@ class KeyIndex {
         }
     }
 
-    void sort() {
+    int[] sort() {
+        int count = 0;
+
+        for (int i = 0; i < k.length; i++) {
+            if (k[i] != 0)
+                count++;
+        }
+
+        int arra[] = new int[count + 1];
+        int index = 0;
+
         if (min != 0) {
             for (int i = 0; i < k.length; i++) {
                 if (k[i] != 0) {
                     for (int j2 = 1; j2 <= k[i]; j2++) {
-                        System.out.println(i - min);
+                        arra[index] = i - min;
+                        index++;
                     }
                 }
             }
@@ -83,20 +114,60 @@ class KeyIndex {
             for (int i = 0; i < k.length; i++) {
                 if (k[i] != 0) {
                     for (int j2 = 1; j2 <= k[i]; j2++) {
-                        System.out.println(i);
+                        arra[index] = i;
+                        index++;
                     }
                 }
             }
         }
+        return arra;
+    }
+}
+
+class Hashing {
+    String[] aux = new String[9];
+    int index = 0;
+    int sum = 0;
+    int consonentCount = 0;
+
+    Hashing(String[] array) {
+        String tempString = "";
+
+        // ST1E89B8A32
+        for (int i = 0; i < array.length; i++) {
+            tempString = array[i];
+            int temp = 0;
+
+            for (int j = 0; j < tempString.length(); j++) {
+                char ch = tempString.charAt(j);
+
+                if (ch >= 'A' && ch <= 'Z') {
+                    if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                        continue;
+                    else
+                        consonentCount++;
+                }
+            }
+
+            for (int j = 0; j < tempString.length(); j++) {
+                char ch = tempString.charAt(j);
+
+                if (ch >= 'A' && ch <= 'Z')
+                    continue;
+                else
+                    sum += (ch - 48);
+            }
+            index = ((consonentCount * 24) + sum) % 9;
+
+            if (aux[index] != array[i]) {
+                aux[index] = array[i];
+            }
+        }
     }
 
-    public static void main(String args[]) {
-        int[] array = { 4, -2, 3, -4, 7, 4 };
-        // int[] array = { 2, 3, 4, 4, 9, 5 };
-
-        KeyIndex keyIndex = new KeyIndex(array);
-        keyIndex.sort();
-        System.out.println(keyIndex.search(1));
+    void print() {
+        for (String string : aux) {
+            System.out.println(string);
+        }
     }
-
 }
