c29d1906b48d4f51aeba3e78306e30a21b3d84b6
arionrefat <arionrefat@gmail.com>
2021-06-01 17:43:26 +0600

    Updated game logic

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,18 +1,35 @@
 'use strict';
 
 const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 console.log(number);
 
 const clickValue = function () {
     const guess = (document.querySelector('.guess').value);
 
     if (!guess)
-        document.querySelector('.message').textContent = 'No number';
+        document.querySelector('.message').textContent = 'No number 😓';
     else {
-        if (guess > number) document.querySelector('.message').textContent = 'Guessed a bit High';
-        else if (guess < number) document.querySelector('.message').textContent = 'Guessed a bit Low';
+        if (guess > number) {
+            if (score > 0) {
+                document.querySelector('.message').textContent = 'Guessed High';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+            else
+                document.querySelector('.message').textContent = 'You Lose!🤯';
+        }
+        else if (guess < number) {
+            if (score > 0) {
+                document.querySelector('.message').textContent = 'Guessed low';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+            else
+                document.querySelector('.message').textContent = 'You Lose!🤯';
+        }
         else {
-            document.querySelector('.message').textContent = 'Correct answer!';
+            document.querySelector('.message').textContent = 'Correct answer!🎉';
             document.querySelector('.number').textContent = guess;
         }
     }
