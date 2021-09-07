e6e77d5198710c412ccf6bda812e7b7d85fbe5c9
arionrefat <arionrefat@gmail.com>
2021-09-08 00:47:55 +0600

    Updated Codes

--- Javascript/07-Pig-Game/starter/script.js
+++ Javascript/07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Score elements so we can use them later
+const player0EL = document.querySelector('.player--1')
+const player1EL = document.querySelector('.player--0')
 const score0El = document.querySelector('#score--0') // # is the selector or ID and . is the selector for class
 const score1El = document.getElementById('score--1') //this is a bit faster
 const current0EL = document.getElementById('current--0') //this is a bit faster
@@ -14,7 +16,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden')
 
-const score = [0,0];
+const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -30,11 +32,19 @@ diceRoll.addEventListener('click', function() {
     if (dice != 1) {
         // add the dice number to the currentscore
         currentScore += dice;
-        current0EL.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }
     else {
         //switched to the next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0EL.classList.toggle('player--active');
+        player1EL.classList.toggle('player--active');
     }
 
+})
+
+diceHold.addEventListener('click', function(){
+    //first add current score to active player's score
 })
